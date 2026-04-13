0fecb421ac5427d3630b16b673953118112179d4
jithendra1226 <23kb5a0222@nbkrist.org>
2026-04-13 21:02:09 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jithendra Kanaparthi",
+    author_email="23kb5a0222@nbkrist.org",
+    packages=find_packages()
+)
